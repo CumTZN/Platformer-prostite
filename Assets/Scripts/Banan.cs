3a52fb1a07a3944d94d728e0ba8617fadb2c3494
CumTZN <artem.nazarov.0208@mail.ru>
2022-05-04 18:15:00 +0300

    fix anim bags

--- Assets/Scripts/Banan.cs
+++ Assets/Scripts/Banan.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class Banan : Entity
 {
-    private float speed = 3.5f;
     private Vector3 dir;
     private SpriteRenderer sprite;
+
+    private void Awake()
+    {
+        sprite = GetComponentInChildren<SpriteRenderer>();
+    }
 
     private void Start()
     {
@@ -28,9 +32,12 @@ public class Banan : Entity
     }
     private void Move()
     {
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position + transform.up * 0.1f + transform.right * dir.x * 0.7f, 0.1f);
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position + transform.up * 0.1f + transform.right * dir.x * 0.7f, - 0.1f);
 
-        if (colliders.Length > 0 ) dir *= -1f;
+        if (colliders.Length > 0) dir *= -1f;
         transform.position = Vector3.MoveTowards(transform.position, transform.position + dir, Time.deltaTime);
+        sprite.flipX = dir.x < 0.0f;
     }
+
+
 }
