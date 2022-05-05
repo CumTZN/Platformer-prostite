7c0e5dbdd93f50345f44d8144fe6df1ca41d9363
CumTZN <artem.nazarov.0208@mail.ru>
2022-05-05 19:17:06 +0300

    level menu + game post
    Главное меню , меню с уровнями ( прокручиваются)

---
+++ Assets/Scripts/LevelSelector.cs
@@ -0,0 +1,27 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelSelector : MonoBehaviour
+{
+    public Button[] levels;
+
+    private void Start()
+    {
+        int levereached = PlayerPrefs.GetInt("levelreached", 1);
+
+        for (int i = 0; i < levels.Length; i++)
+               if (i + 1 > levereached)
+                levels[i].interactable = false;
+    }
+
+    public void Select(int numberInBuild)
+    { 
+        SceneManager.LoadScene(numberInBuild);
+    }
+    
+}
+
